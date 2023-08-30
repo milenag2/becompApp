b5a1bd672cdde58633e1d893a021ab7fed331018
deMelo1 <130903363+deMelo1@users.noreply.github.com>
2023-08-29 21:35:28 -0300

    Add files via upload

---
+++ screens/inicio/App.js
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState, useEffect} from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+    console.disableYellowBox = true;
+    return (
+    <View style={{backgroundColor: 'black', width: '100%', height:'100%'}}>
+        <Text style={{color:'white', padding: 30}}>ola</Text>
+    </View>
+    )
+}
+
+const style = StyleSheet.create({
+
+})
